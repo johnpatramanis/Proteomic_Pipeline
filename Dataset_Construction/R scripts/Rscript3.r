ef16d5c89a4d2b34cca867b3507d6f3a1362691e
Ioannis Patramanis <32954442+johnpatramanis@users.noreply.github.com>
2021-06-10 14:46:56 +0200

    Add files via upload

---
+++ Dataset_Construction/R scripts/Rscript3.r
@@ -0,0 +1,76 @@
+args = commandArgs(trailingOnly=TRUE)
+#command to run:    Rscript Rscript1.r 'sample_file' 'Starts_file' 'EIT_folder_location' 'output_location'
+#command to run(from Dataset_Construction folder):    Rscript R\ scripts/Rscript3.r ./Workspace/BLASTED_GENES/DENISOVA_DENISOVA_ADNA_FRMT_AMELX_spliced.blast Workspace/PROTEINS_FASTAS/
+
+library(ShortRead)
+
+
+fas<-args[1]
+
+#Get gene and sample name
+name<-strsplit(gsub(".blast", "", fas), "/")[[1]][length(strsplit(gsub(".fa", "", fas), "/")[[1]])]
+gene<-strsplit(name,"_")[[1]][length(strsplit(name,"_")[[1]])-1]#gene name from file name
+samp<-paste(strsplit(name,"_")[[1]][1:(length(strsplit(name,"_")[[1]])-2)],collapse='_') # Sample name from file name
+
+
+fout<-paste0(args[2],samp,'_',gene, "_translated.fa")  #name of output fasta (protein)
+h<-paste(c(paste(strsplit(name,"_")[[1]][1:(length(strsplit(name,"_")[[1]])-3)],collapse='_'),gene),collapse='_')# just the sample name
+
+#for each blast file
+blout<-fas   #grab each file
+zz<-pipe(paste0("grep -e \"Identities\" -e \"Query\" -e \"Sbjct\" -e \"Length of query\" ", blout, " | grep -v \"Query=\""))  #create pipe connection object for file
+a<-scan(zz, what="", sep="\n") #scan pipe ?
+close(zz) #
+zz<-pipe(paste0("grep \"letters)\" ", blout)) #new pipe
+len<-scan(zz, what="", sep="\n")  #sequence
+close(zz)
+len<-as.numeric(strsplit(strsplit(len, "\\(")[[1]][2], " ")[[1]][1]) #length of sequence
+separator<-grep("Identities", a) #
+
+if(length(separator)>1){
+    a<-a[(separator[1]+1):(separator[2]-1)]
+    b<-a[grep("Query",a)]
+    tot<-as.numeric(strsplit(b[length(b)], " ")[[1]][length(strsplit(b[length(b)], " ")[[1]])])
+    b<-as.numeric(strsplit(b[1], " ")[[1]][2])
+    a<-a[grep("Sbjct",a)]
+    a<-gsub("Sbjct: (\\d+)( *+)", "", a, perl=TRUE)
+    a<-gsub(" (\\d+)", "", a, perl=TRUE)
+    if(b!=1){
+        a<-c(paste(collapse="", rep("X", b-1)), a)
+		}
+    if(tot<len){
+        a<-c(a, paste(collapse="", rep("X", len-tot)))
+		}
+    a<-paste(collapse="", a)
+    newseq<-AAStringSet(a)
+    names(newseq)<-h
+    writeXStringSet(newseq, fout)
+
+}else{#if separator=0
+    if(length(a)==1|length(a)==0){ #if no results at all! # seems to be called only for samples that lack AMELY, good!
+		a=paste(collapse="", rep("X", 200))
+		newseq<-AAStringSet(a)
+		names(newseq)<-h
+		writeXStringSet(newseq, fout)
+        print(paste0("No Results for 1 BLAST ",h))
+        
+        }else{
+			a<-a[(separator[1]+1):(length(a))]
+			b<-a[grep("Query",a,ignore.case=TRUE)]
+			tot<-as.numeric(strsplit(b[length(b)], " ")[[1]][length(strsplit(b[length(b)], " ")[[1]])])
+			b<-as.numeric(strsplit(b[1], " ")[[1]][2])
+			a<-a[grep("Sbjct",a)]
+			a<-gsub("Sbjct: (\\d+)( *+)", "", a, perl=TRUE)
+			a<-gsub(" (\\d+)", "", a, perl=TRUE)
+			if(b!=1){
+				a<-c(paste(collapse="", rep("X", b-1)), a)
+				}
+			if(tot<len){
+				a<-c(a, paste(collapse="", rep("X", len-tot)))
+				}
+			a<-paste(collapse="", a)
+			newseq<-AAStringSet(a)
+			names(newseq)<-h
+			writeXStringSet(newseq, fout)
+        }        
+}
---
+++ Dataset_Construction/Workspace/PROTEINS_FASTAS/README.txt
@@ -0,0 +1,3 @@
+Protein FASTA format files will be placed here.
+They should correspond to each protein of interest per sample. Note that if anything went wrong in the process a file will have a dummy file in its place (XXXs sequence)
+One very common such case is when trying to isolate AMELY from a female sample that has not Y chromosome thus no AMELY, leading to a protein file with missing sequence.
