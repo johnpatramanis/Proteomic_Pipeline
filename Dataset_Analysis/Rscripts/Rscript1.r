e30aea5ced61088e4a3149b005763a88312e049c
Ioannis Patramanis <32954442+johnpatramanis@users.noreply.github.com>
2021-07-12 16:54:22 +0200

    Add files via upload

---
+++ Dataset_Analysis/Rscripts/Rscript1.r
@@ -0,0 +1,32 @@
+args = commandArgs(trailingOnly=TRUE)
+#command to run:    Rscript Rscript1.r OutputDir REF_dataset 
+#command to run (from Dataset_Construction folder):    Rscript Rscripts/Rscript1.r /Workspace/2_DATASETS/New_Ref_Seq_3_21_1846.fa-1846 Workspace/1_OG_Dataset/New_Ref_Seq_3_21_1846.fa
+
+library(ShortRead)
+
+
+directory=args[1] #The directory location for the new folders to be created 
+
+# This correspond to the protein sequences for the reference samples
+fa<-readAAStringSet(args[2]) #GET it from 1_OG_DATASET
+
+mainDir<-getwd() #get current dir, assumes output dir is a subfodler!
+setwd(file.path(mainDir,directory))
+genes<-readLines("Genes.txt") #In the 2_DATASETS appropriate folder 
+
+
+mainDir <- getwd()
+
+for(i in 1:length(genes)){
+	
+	
+	subDir <- genes[i]
+	dir.create(file.path(mainDir, subDir), showWarnings = FALSE)
+
+	d<-file.path(mainDir,genes[i])
+	setwd(d)
+	curfa<-fa[grep(genes[i], names(fa))]  ### biostring with only one gene (all fasta seqs that have the gene name in their name), all sample sequences tied with their sample name
+	
+
+	writeXStringSet(curfa, paste0(genes[i], "_o.fa")) 
+}
