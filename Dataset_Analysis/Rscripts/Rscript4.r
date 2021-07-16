bf6ec21036b877f696aeaed85451e8b0e1b4a200
Ioannis Patramanis <32954442+johnpatramanis@users.noreply.github.com>
2021-07-16 12:21:33 +0200

    Add files via upload

---
+++ Dataset_Analysis/Rscripts/Rscript4.r
@@ -0,0 +1,48 @@
+#command to run:    Rscript Rscript3.r  
+#command to run (from Dataset_Construction folder):    Rscript Rscripts/Rscript4.r /Workspace/2_DATASETS/New_Ref_Seq_3_21_1846.fa-H_antecessor-1846
+
+args = commandArgs(trailingOnly=TRUE)
+
+
+library("phyclust")
+
+directory<-args[1]
+
+mainDir<-getwd() #get current dir, assumes output dir is a subfodler!
+setwd(file.path(mainDir,directory))
+
+genes<-readLines("Genes.txt")
+
+
+for(g in 1:length(genes)){
+
+    setwd(file.path(mainDir,directory,genes[g]))
+    fasta_data=read.fasta(paste0("./",genes[g],"_aln_e.fa"), code.type ="AMINO_ACID",aligned=TRUE)
+    names=fasta_data$seqname
+    
+    for (i in 1:length(names)){
+        names[i]=paste0(substring(names[i], 1, nchar(names[i])),"\t")
+    }
+
+
+    write.phylip( fasta_data$org , paste0("./",genes[g],"_aln_e.phy") ,code.type = "AMINO_ACID",seqname=names, width.seqname =100)
+
+# backup method if errors
+# write.phylip.format( fasta_data$org.code , paste0("./",genes[g],"_aln_e.phy"),seqname=names, width.seqname =10)
+}
+
+
+
+######### Turn the concatenated one into Phyl as well
+
+
+setwd(file.path(mainDir,directory,'CONCATINATED'))
+concat_fasta=read.fasta("CONCATINATED_o.fa", code.type ="AMINO_ACID",aligned=TRUE)
+
+
+names=concat_fasta$seqname 
+for (i in 1:length(names)){
+    names[i]=paste0(substring(names[i], 1, nchar(names[i])),"\t")
+}
+
+write.phylip( concat_fasta$org , "./CONCATINATED_aln_e.phy" ,code.type = "AMINO_ACID",seqname=names, width.seqname =100)
